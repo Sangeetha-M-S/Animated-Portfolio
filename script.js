c1fd905ecb426540ecf25d0428a2147dd74672be
Sangeetha-M-S <91152290+Sangeetha-M-S@users.noreply.github.com>
2023-05-19 16:42:12 +0530

    13th commit

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 50;
+        let offset = sec.offsetTop - 0;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
@@ -35,7 +35,7 @@ window.onscroll = () => {
     //sticky header
     let header = document.querySelector('header');
 
-    header.classList.toggle('sticky', window.scrollY > 50);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
     // remove toggle icon and navbar when click navbar links (scroll)
 
